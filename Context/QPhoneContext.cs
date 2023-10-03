f1328f635208430266b0efb2fd40b31951e5b6ff
brad1141 <bradlamontagne1141@gmail.com>
2023-10-03 12:42:31 -0400

    base line

--- Context/QPhoneContext.cs
+++ Context/QPhoneContext.cs
@@ -5,12 +5,6 @@ namespace QPhone.Service.Context
 {
 	public class QPhoneContext : DbContext
 	{
-		private readonly string connectionString;
-		public DbSet<Event> Event { get; set; }
-
-		public QPhoneContext(string connectionString)
-		{
-			this.connectionString = connectionString;
-		}
+		
 	}
 }
